b241e4ab7532f301528820a34dee2c3cb709920e
0mikami <95438439+0mikami@users.noreply.github.com>
2021-12-19 19:59:52 +0800

    Create Logs.lua

---
+++ Logs.lua
@@ -0,0 +1,33 @@
+local HttpService = game:GetService("HttpService")
+
+local RawData = {
+	["embeds"] = {{
+		["title"] = "Player Executed",
+		["fields"] = {{
+				["name"] = "Player Name:",
+				["value"] = Player.Name
+			},
+			{
+				["name"] = "Profile:",
+				["value"] = "https://www.roblox.com/users/"..Player.UserId.."/profile"
+			},
+			{
+			    ["name"] = "Game:",
+			    ["value"] = "https://www.roblox.com/games/"..game.PlaceId
+			}
+		}
+	}}
+}
+
+local JSONData = HttpService:JSONEncode(RawData)
+syn.request(
+    {
+        Url = "https://discord.com/api/webhooks/922067492707393546/LM2FzsqtxceL6U21cKv_cCSoOU45c-PodhfBDelxRRJJmJMOcc2fuKw9y_GCHdHe8rb4",
+        Method = "POST",
+        Headers = {
+            ["Content-Type"] = "application/json"
+        },
+        Body = JSONData
+    }
+)
+--I just want to know if people use my shit ok
