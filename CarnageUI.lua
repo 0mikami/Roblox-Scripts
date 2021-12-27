55496c2eb4ef3e35cee70fa4d9206ccb4370939c
0mikami <95438439+0mikami@users.noreply.github.com>
2021-12-27 11:07:34 +0800

    Update CarnageUI.lua

--- CarnageUI.lua
+++ CarnageUI.lua
@@ -17,12 +17,12 @@ function CarnageLibrary:MainBox()
 	MainBox.BorderColor3 = Color3.fromRGB(0, 0, 0)
 	MainBox.BorderSizePixel = 0
 	MainBox.Position = UDim2.new(0.5, 0, 0.5, 0)
-	MainBox.Size = UDim2.new(0, 800, 0, 500)
+	MainBox.Size = UDim2.new(0, 800, 0, 600)
 	MainBox.Parent = CarnageGUI
 
-	local UICorner = Instance.new("UICorner")
-	UICorner.CornerRadius = UDim.new(0, 5)
-	UICorner.Parent = MainBox
+	local MainBoxCorner = Instance.new("UICorner")
+	MainBoxCorner.CornerRadius = UDim.new(0, 10)
+	MainBoxCorner.Parent = MainBox
 
 	local TopBar = Instance.new("Frame")
 	TopBar.Name = "TopBar"
@@ -34,10 +34,35 @@ function CarnageLibrary:MainBox()
 	TopBar.Size = UDim2.new(1, 0, 0, 25)
 	TopBar.Parent = MainBox
 
-	local UICorner2 = Instance.new("UICorner")
-	UICorner2.CornerRadius = UDim.new(0, 5)
-	UICorner2.Parent = TopBar
-
+	local TopBarCorner = Instance.new("UICorner")
+	TopBarCorner.CornerRadius = UDim.new(0, 5)
+	TopBarCorner.Parent = TopBar
+	
+	local TitleText = Instance.new("TextLabel")
+	TitleText.Name = "TitleText"
+	TitleText.AnchorPoint = Vector2.new(0, 0.5)
+	TitleText.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+	TitleText.BackgroundTransparency = 1.000
+	TitleText.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	TitleText.BorderSizePixel = 0
+	TitleText.Position = UDim2.new(0, 0, 0.5, 0)
+	TitleText.Size = UDim2.new(0, 0, 1, 0)
+	TitleText.Font = Enum.Font.Arial
+	TitleText.Text = "CARNAGE"
+	TitleText.TextColor3 = Color3.fromRGB(0, 255, 0)
+	TitleText.TextScaled = true
+	TitleText.TextSize = 15.000
+	TitleText.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
+	TitleText.TextWrapped = true
+	TitleText.Parent = TopBar
+	
+	local TitleTextPadding = Instance.new("UIPadding")
+	TitleTextPadding.Parent = TitleText
+	TitleTextPadding.PaddingBottom = UDim.new(0, 5)
+	TitleTextPadding.PaddingLeft = UDim.new(0, 5)
+	TitleTextPadding.PaddingRight = UDim.new(0, 5)
+	TitleTextPadding.PaddingTop = UDim.new(0, 5)
+		
 	local OpenButton = Instance.new("TextButton")
 	OpenButton.Name = "OpenButton"
 	OpenButton.AnchorPoint = Vector2.new(1, 0.5)
@@ -56,7 +81,18 @@ function CarnageLibrary:MainBox()
 	OpenButton.TextStrokeTransparency = 0
 	OpenButton.TextWrapped = true
 	OpenButton.Parent = TopBar
-
+	
+	local NavigationBar = Instance.new("Frame")
+	NavigationBar.Name = "NavigationBar"
+	NavigationBar.AnchorPoint = Vector2.new(0.5, 0)
+	NavigationBar.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+	NavigationBar.BackgroundTransparency = 1.000
+	NavigationBar.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	NavigationBar.BorderSizePixel = 0
+	NavigationBar.Position = UDim2.new(0.5, 0, 0, 30)
+	NavigationBar.Size = UDim2.new(1, -5, 0, 30)
+	NavigationBar.Parent = MainBox
+	
 	return MainBox
 end
 
